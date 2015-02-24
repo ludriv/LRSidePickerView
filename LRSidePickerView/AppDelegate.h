29d4587f2b0d8ac3da3ba92a5df3f0134bdbcaa4
Ludovic Riviere <ludovic.riviere2@gmail.com>
2015-02-24 09:56:27 +0100

    Project initialization

---
+++ LRSidePickerView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LRSidePickerView
+//
+//  Created by Ludovic Riviere on 24/02/2015.
+//  Copyright (c) 2015 ludovicriviere.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LRSidePickerView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LRSidePickerView
+//
+//  Created by Ludovic Riviere on 24/02/2015.
+//  Copyright (c) 2015 ludovicriviere.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
